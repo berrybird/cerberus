a2fa8dfdc1e128a2f88daf0d6e59270ad7c24fca
Miodrag Tokić <miki@berrybird.io>
2015-06-01 12:47:11 +0200

    Use the "autoload-dev" Composer schema property
    Instead of adding test source files manually to the Composer loader, use
    the "autoload-dev" Composer schema property.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,4 @@
 
 date_default_timezone_set('UTC');
 
-$loader = require realpath(__DIR__ . '/../vendor/autoload.php');
-
-$loader->addPsr4('Berrybird\\Cerberus\\', __DIR__ . '/src');
+require realpath(__DIR__ . '/../vendor/autoload.php');
