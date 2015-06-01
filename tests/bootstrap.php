03036e70660d8f60630a235f83eaf27bf027bb94
Miodrag Tokić <miki@berrybird.io>
2015-06-01 12:44:47 +0200

    Fix vendor path in the test bootstrap file
    References f56d59b

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,6 +2,6 @@
 
 date_default_timezone_set('UTC');
 
-$loader = require realpath(__DIR__ . '/../lib/autoload.php');
+$loader = require realpath(__DIR__ . '/../vendor/autoload.php');
 
 $loader->addPsr4('Berrybird\\Cerberus\\', __DIR__ . '/src');
